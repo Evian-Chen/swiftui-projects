c9b8ab9084d04b57fd7072f990deb981cf5d6854
Yan-he Chen <paramecium128@gmail.com>
2025-02-20 13:11:34 +0800

    feat: add calculator files via upload

---
+++ caculator/caculator/caculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  caculatorApp.swift
+//  caculator
+//
+//  Created by Mac25 on 2025/2/20.
+//
+
+import SwiftUI
+
+@main
+struct caculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ caculator/caculatorTests/caculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  caculatorTests.swift
+//  caculatorTests
+//
+//  Created by Mac25 on 2025/2/20.
+//
+
+import Testing
+@testable import caculator
+
+struct caculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
