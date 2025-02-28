6925855f1ed53e3ad2606046e23217455cf594b7
EvianChen <paramecium128@gmail.com>
2025-03-01 00:46:25 +0800

    feat: add an alert before showing MainView

--- PlacementAnalysis/PlacementAnalysis/ContentView.swift
+++ PlacementAnalysis/PlacementAnalysis/ContentView.swift
@@ -12,25 +12,78 @@ struct button: ButtonStyle {
         configuration.label
             .font(.system(size: 40))
             .fontWeight(.semibold)
-            .background(Color.orange) // Background color of the square
-            .cornerRadius(10) // Optional: Round the corners of the square
-            .padding(10)
+            .background(Color.green)
+            .cornerRadius(10)
+            .padding(15.0)
+            .clipShape(Capsule())
+    }
+}
+
+struct Placement: View {
+    var placementTapped = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                // 下拉式選單直接選擇大樓（直接同步公尺）
+                // 輸入公尺
+            } //  HStack
+        } //  VStack
+    }
+}
+
+struct NormalUnitTransition: View {
+    var body: some View {
+        Text("Hello")
     }
 }
 
 struct ContentView: View {
+    @State private var isAllowed = false
+    @State private var showAlert = true
+    
+    var body: some View {
+        VStack {
+            if isAllowed {
+                MainView()
+            } else {
+                Color.white.ignoresSafeArea()
+            }
+        }
+        .alert("Welcome!", isPresented: $showAlert) {
+            Button("OK") {
+                isAllowed = true
+            }
+            Button("No") {
+                exit(0)
+            }
+        } message: {
+            Text("Do you agree to use this app?")
+        }
+}
+
+struct MainView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Button("落點分析") {}
-                    .buttonStyle(button())
+                NavigationLink("落點分析") {
+                    Placement()
+                }
+                .buttonStyle(button())
                 
-                Button("一般單位轉換") {}
-                    .buttonStyle(button())
+                NavigationLink("一般單位轉換") {
+                    NormalUnitTransition()
+                }
+                .buttonStyle(button())
+                
             } // VStack
+            .navigationTitle("Main")
             .border(Color.red)
             
+            
         } // NavigationStack
+        .border(Color.red)
+    }
     }
 }
 
