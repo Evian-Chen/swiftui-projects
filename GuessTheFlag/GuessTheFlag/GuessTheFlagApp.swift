8eb5957538a4b7c41515e460ac078c3ee141daf6
Yan-he Chen <paramecium128@gmail.com>
2025-02-28 23:17:04 +0800

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by mac03 on 2025/2/28.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
