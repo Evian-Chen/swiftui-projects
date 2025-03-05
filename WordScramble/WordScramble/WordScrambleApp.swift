d2a97323b9810acc2bc1e9eaf07828512c468270
Evian Chen <paramecium128@gmail.com>
2025-03-05 16:17:02 +0800

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Mac25 on 2025/3/5.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
