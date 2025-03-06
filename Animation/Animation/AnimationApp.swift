e4f7933772029bb3442f1bedce54bb6f4b385d48
Evian Chen <paramecium128@gmail.com>
2025-03-06 12:06:53 +0800

    Add files via upload

---
+++ Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Mac25 on 2025/3/6.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
