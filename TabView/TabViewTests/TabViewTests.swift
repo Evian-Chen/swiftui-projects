f31f0621998b24aa346477cf8c15847675a60345
Evian Chen <paramecium128@gmail.com>
2025-03-13 12:11:54 +0800

    Add files via upload

---
+++ TabView/TabViewTests/TabViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewTests.swift
+//  TabViewTests
+//
+//  Created by Mac25 on 2025/3/13.
+//
+
+import Testing
+@testable import TabView
+
+struct TabViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
