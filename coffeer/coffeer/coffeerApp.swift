88beaf9153ec5c9cd1dd192a1ea7a8319f66d23d
EvianChen <paramecium128@gmail.com>
2025-03-15 23:22:30 +0800

    feat: add coffeer file(undone)

---
+++ coffeer/coffeer/coffeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeerApp.swift
+//  coffeer
+//
+//  Created by mac03 on 2025/3/15.
+//
+
+import SwiftUI
+
+@main
+struct coffeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ coffeer/coffeerTests/coffeerTests.swift
@@ -0,0 +1,17 @@
+//
+//  coffeerTests.swift
+//  coffeerTests
+//
+//  Created by mac03 on 2025/3/15.
+//
+
+import Testing
+@testable import coffeer
+
+struct coffeerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
