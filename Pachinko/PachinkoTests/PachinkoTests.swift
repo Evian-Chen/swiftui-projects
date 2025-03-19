27ee9937b6d2ecbd601d5e0b7d5b0d4fd33206d0
Evian Chen <paramecium128@gmail.com>
2025-03-19 15:51:23 +0800

    Add files via upload

---
+++ Pachinko/PachinkoTests/PachinkoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PachinkoTests.swift
+//  PachinkoTests
+//
+//  Created by Mac25 on 2025/3/19.
+//
+
+import Testing
+@testable import Pachinko
+
+struct PachinkoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
