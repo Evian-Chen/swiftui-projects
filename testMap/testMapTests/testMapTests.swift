5fe12c362727f6771e7e7710ac6d9368d57051e7
EvianChen <paramecium128@gmail.com>
2025-03-21 15:37:09 +0800

    feat: add GoogleMapTest iOS google map sdk sample

---
+++ testMap/testMapTests/testMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  testMapTests.swift
+//  testMapTests
+//
+//  Created by mac03 on 2025/3/20.
+//
+
+import Testing
+@testable import testMap
+
+struct testMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
