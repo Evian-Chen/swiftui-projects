bd726121857c73c7aa6893c4f2983e29319d9fb1
Evian Chen <paramecium128@gmail.com>
2025-03-27 12:01:06 +0800

    add ninja game (credit by TA Jimmy)

---
+++ Ninja-undone/NinjaTests/NinjaTests.swift
@@ -0,0 +1,17 @@
+//
+//  NinjaTests.swift
+//  NinjaTests
+//
+//  Created by hpclab on 2025/3/24.
+//
+
+import Testing
+@testable import Ninja
+
+struct NinjaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
