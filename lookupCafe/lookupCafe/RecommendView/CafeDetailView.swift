c0ca989b0782582c1f68bcab3c0a89ba8e040940
EvianChen <paramecium128@gmail.com>
2025-05-05 23:21:19 +0800

    feat: working on map

--- lookupCafe/lookupCafe/RecommendView/CafeDetailView.swift
+++ lookupCafe/lookupCafe/RecommendView/CafeDetailView.swift
@@ -6,6 +6,35 @@
 //
 
 import SwiftUI
+import GoogleMaps
+
+struct CafeMapPreviewView: UIViewRepresentable {
+    var address: String
+    
+    func makeUIView(context: Context) -> GMSMapView {
+        let camera = GMSCameraPosition.camera(
+            withLatitude: 25.034012,  // 台北101
+            longitude: 121.564461,
+            zoom: 15
+        )
+        let mapView = GMSMapView(frame: .zero, camera: camera)
+
+        // 加一個 marker 做示範
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 25.034012, longitude: 121.564461)
+        marker.title = "Taipei 101"
+        marker.snippet = "台北市信義區"
+        marker.map = mapView
+
+        return mapView
+    }
+    
+//    func addr2Coordinat()
+
+    func updateUIView(_ uiView: GMSMapView, context: Context) {
+        // 這裡暫時不需要寫
+    }
+}
 
 extension Image {
     func iconModifier() -> some View {
