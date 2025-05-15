e507c4728725d15dfaee0aeec1c48c2b634f1dfd
EvianChen <paramecium128@gmail.com>
2025-05-15 23:28:09 +0800

    feat: launch screen

--- Bookworm/Bookworm/BookwormApp.swift
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -12,8 +12,39 @@ import SwiftUI
 struct BookwormApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchView()
         }
         .modelContainer(for: Book.self)
+    }
+}
+
+
+struct LaunchView: View {
+    @State private var isActive =  false
+    @State private var opacity = 1.0
+    
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            VStack {
+                Image("Images")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                Text("Welcome to my App")
+                    .font(.headline)
+                    .padding()
+            }
+            .opacity(opacity)
+            .onAppear {
+                withAnimation(.easeInOut(duration: 3.0)) {
+                    self.opacity = 0.0
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now()+3) {
+                    self.isActive = true
+                }
+            }
+        }
     }
 }
